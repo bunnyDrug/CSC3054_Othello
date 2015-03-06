82e777523e86bf869375082519256a3d3fce9e16
Don <aries2580@gmail.com>
2015-03-06 17:25:05 +0000

    Simple Grid working
    A basic grid displayed on the screen and coloured in. It responds to
    click events and reports the position in the grid that was tapped.

--- OthelloApp/app/src/main/java/com/example/don/othello/MainActivity.java
+++ OthelloApp/app/src/main/java/com/example/don/othello/MainActivity.java
@@ -1,19 +1,71 @@
 package com.example.don.othello;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+
+    // new grid view
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //TODO: remember to change this back to the main activity after testing
+        setContentView(R.layout.grid_fragment);
+
+        // create a new grid and populate it
+        testGrid();
+
+        // colour the grind with the 100 shade orange
+        gridView.setBackgroundColor(Color.parseColor("#FFE0B2"));
+
+        //start click listener
+        setOnClickListener();
+
     }
 
+    public void testGrid() {
+        populateGridView();
+    }
+
+    /**
+     * Creates a new grad view and displays it to the screen<br>
+     * The grid is populated with numerical values from 0 to 64.
+     */
+    private void populateGridView() {
+        // create an array of 64 Strings
+        // Elements go from "0" to "63"
+        String[] gridFill = new String[64];
+        for (int i = 0; i<gridFill.length; i++) {
+            gridFill[i] = Integer.toString(i);
+        }
+
+        // assign the grid view variable to the grid view on the layout
+        gridView = (GridView) findViewById(R.id.gridView1);
+
+        // creates an adapter and fills it with the array created
+        ArrayAdapter<String> adapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_list_item_1, gridFill);
+        gridView.setAdapter(adapter);
+    }
+
+    public void setOnClickListener() {
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getBaseContext(), "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
