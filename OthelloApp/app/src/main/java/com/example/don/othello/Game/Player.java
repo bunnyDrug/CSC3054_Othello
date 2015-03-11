6b3db33cfcf9f913e6d0528bf1e8b45522b1213c
Don <aries2580@gmail.com>
2015-03-11 15:58:16 +0000

    Player class comments and removed magic number
    Commented the class so it is clear. Removed the tick timer which was a
    magic number.

--- OthelloApp/app/src/main/java/com/example/don/othello/Game/Player.java
+++ OthelloApp/app/src/main/java/com/example/don/othello/Game/Player.java
@@ -12,6 +12,9 @@ import android.widget.TextView;
 
 public class Player {
 
+    // 1 second
+    private final long TICK_INTERVAL = 1000;
+
     private boolean validMovesAvailable = true;
 
     // TODO: read this in from a settings page - remove hard coded value
@@ -23,11 +26,23 @@ public class Player {
     int score;
     private TextView textViewScore;
 
-
-    boolean timedGame;
     private TextView textViewTimer;
     private CountDownTimer countDownTimer;
 
+    /**
+     * A class that represents the players in the game. This player class is
+     * responsible for holding its own timer and score. Any updates to these
+     * stored values should be printed to the screen using the print methods
+     * @param name the user input name
+     * @param textViewName the text area on the screen where the name should
+     *                     appear
+     * @param score a users score - made up from the number of tiles on the
+     *              board that represent their colour
+     * @param textViewScore the text area that displays the score value to a
+     *                      player
+     * @param textViewTimer the text area that displays the time remaining for
+     *                      a player.
+     */
     public Player(String name, TextView textViewName,
                   int score, TextView textViewScore,
                   TextView textViewTimer) {
@@ -41,19 +56,37 @@ public class Player {
         this.textViewTimer = textViewTimer;
     }
 
+    /**
+     * Set the number of minutes each player has before a game is over
+     * @param timer
+     */
+    public void setGameTimer(long timer) {
+        playerTimeLimit = timer;
+    }
+
+    /**
+     * Creates a new countdown timer for the player.
+     * Each tick updates the text on the
+     * display - the text view passed in via the constructor.
+     *
+     * After each tick the time remaining is stored in a variable so it may be
+     * resumed after it is paused.
+     */
     public void startTimer() {
-        countDownTimer = new CountDownTimer(playerTimeLimit, 1000) {
+        countDownTimer = new CountDownTimer(playerTimeLimit, TICK_INTERVAL) {
             public void onTick(long millisUntilFinished) {
                 textViewTimer.setText("Time left: " + millisUntilFinished / 1000 + " seconds");
                 playerTimeLimit = millisUntilFinished;
             }
-
             public void onFinish() {
                 textViewTimer.setText("Game over");
             }
         }.start();
     }
 
+    /**
+     * Stops the player timer
+     */
     public void pauseTimer() {
         if (countDownTimer == null) {
             Log.d("Player class","no timer to pause");
@@ -64,5 +97,13 @@ public class Player {
 
     public void printName() {
         textViewName.setText(name);
+    }
+
+    public void updateScore(int score) {
+        this.score = score;
+    }
+
+    public void printScore() {
+        textViewScore.setText(score);
     }
 }
