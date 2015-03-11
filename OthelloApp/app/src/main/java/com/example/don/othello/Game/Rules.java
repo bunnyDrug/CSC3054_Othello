68681d65fef15de0ac61b941c94330f9380893d8
Don <aries2580@gmail.com>
2015-03-11 16:00:03 +0000

    Rules class
    Start of the rules class

---
+++ OthelloApp/app/src/main/java/com/example/don/othello/Game/Rules.java
@@ -0,0 +1,16 @@
+package com.example.don.othello.Game;
+
+/**
+ * Created by don on 11/03/2015.
+ */
+public class Rules {
+
+    private Rules() {
+
+    }
+
+    public static boolean validPiecePlacement() {
+
+        return false;
+    }
+}
