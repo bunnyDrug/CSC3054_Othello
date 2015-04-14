559c41bb75676d07f56bf692c25c89c565ab06c4
Don <aries2580@gmail.com>
2015-04-14 11:09:41 +0100

    rename MainActivity -> GameActivity

--- OthelloApp/app/src/main/java/com/example/don/othello/GameActivity.java
+++ OthelloApp/app/src/main/java/com/example/don/othello/GameActivity.java
@@ -3,12 +3,10 @@ package com.example.don.othello;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.don.othello.Game.OthelloSystem;
 
-public class MainActivity extends ActionBarActivity {
+public class GameActivity extends ActionBarActivity {
 
     OthelloSystem othelloSystem;
 
@@ -37,28 +35,5 @@ public class MainActivity extends ActionBarActivity {
 
         othelloSystem = new OthelloSystem(this, false);
         othelloSystem.startGame(topPlayer, bottomPlayer);
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.game_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
