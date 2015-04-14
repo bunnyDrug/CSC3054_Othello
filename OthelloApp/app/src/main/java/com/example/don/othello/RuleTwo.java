1ac7179262ea5a7a02ec2d7fdfa91db114266be9
Don <aries2580@gmail.com>
2015-04-14 11:16:36 +0100

    remove menu code
    from activity's that do not need to display a menu

--- OthelloApp/app/src/main/java/com/example/don/othello/RuleTwo.java
+++ OthelloApp/app/src/main/java/com/example/don/othello/RuleTwo.java
@@ -1,18 +1,17 @@
 package com.example.don.othello;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class RuleTwo extends ActionBarActivity {
 
     Button btnpre,btnnex;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,31 +35,5 @@ public class RuleTwo extends ActionBarActivity {
                 startActivity(intent);
             }
         });
-
-
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_rule_two, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- OthelloApp/app/src/main/java/com/example/don/othello/SpalshScreen/Othello.java
+++ OthelloApp/app/src/main/java/com/example/don/othello/SpalshScreen/Othello.java
@@ -1,11 +1,8 @@
 package com.example.don.othello.SpalshScreen;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageButton;
+import android.support.v7.app.ActionBarActivity;
 
 import com.example.don.othello.MenuPage;
 import com.example.don.othello.R;
@@ -31,28 +28,5 @@ public class Othello extends ActionBarActivity {
         Timer splash = new Timer();
         splash.schedule(task,7000);
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_othello, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
