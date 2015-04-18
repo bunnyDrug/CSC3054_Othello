f0bf865f1de7b93182a26e0a230ce1fedddf6cc8
Don <aries2580@gmail.com>
2015-04-18 19:34:21 +0100

    Refactor HighScores class
    Comments and code tidying.

--- OthelloApp/app/src/main/java/com/CSC/othello3054/game/HighScores.java
+++ OthelloApp/app/src/main/java/com/CSC/othello3054/game/HighScores.java
@@ -19,20 +19,33 @@ public class HighScores extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_high_scores);
 
-        ShareLinkContent content = new ShareLinkContent.Builder()
-                .setContentUrl(Uri.parse("http://bunnydrug.github.io/CSC3054_Othello/"))
-                .setContentTitle("Othello - Top 5 High Scores")
-                .setContentDescription("Can you beat my top scores?\n" + getScores())
-                .build();
-
-        ShareButton shareButton = (ShareButton)findViewById(R.id.share_view);
-        shareButton.setShareContent(content);
-
         printScores(getScores());
+
+        facebookShareInit();
+    }
+
+    /**
+     * Sets up the required stuff to allow content to be shared to a users
+     * facebook timeline.
+     * The facebook link that is shared is the game repo.
+     */
+    private void facebookShareInit() {
+        ShareButton shareButton = (ShareButton)findViewById(R.id.share_view);
+
+        if (getScores().equals("No Scores to display")){
+            shareButton.setEnabled(false);
+        } else {
+            ShareLinkContent content = new ShareLinkContent.Builder()
+                    .setContentUrl(Uri.parse("http://bunnydrug.github.io/CSC3054_Othello/"))
+                    .setContentTitle("Othello - Top 5 High Scores")
+                    .setContentDescription("Can you beat my top scores?\n" + getScores())
+                    .build();
+
+            shareButton.setShareContent(content);
+        }
     }
 
     /**
@@ -43,6 +56,12 @@ public class HighScores extends ActionBarActivity {
         txtViewTextScores.setText(scores);
     }
 
+    /**
+     * Gets the top 5 scores from the database of players returned as a string.
+     * If there are no scores to return, an error string is return but no
+     * exception thrown.
+     * @return String of scores from the database.
+     */
     private String getScores() {
         String scores;
         if (dbHelper.databaseToString(dbHelper.getWritableDatabase()).equals("")) {
