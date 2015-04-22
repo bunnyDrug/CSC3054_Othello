02f3330c129c7ab07911fa5907d01157e1a18ece
LtIncognito <chris.d.lavery@gmail.com>
2015-04-22 23:40:58 +0100

    Strings added
    Strings for the slide rule bar

--- OthelloApp/app/src/main/java/com/CSC/othello3054/game/Rules/Classes/RuleThree.java
+++ OthelloApp/app/src/main/java/com/CSC/othello3054/game/Rules/Classes/RuleThree.java
@@ -9,20 +9,31 @@ import android.widget.Button;
 import com.CSC.othello3054.game.MainMenu;
 import com.CSC.othello3054.game.R;
 
-
 public class RuleThree extends ActionBarActivity {
+
     Button home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rule_three);
+/*
+Button to return to the main menu page instead of hitting the back button 4 times
+ */
+
+
         home = (Button)findViewById(R.id.btnHome);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RuleThree.this, MainMenu.class);
                 startActivity(intent);
+                finish();
             }
         });
+
+
+    //    no need for a previous button as we should be making use of the
+    //    android design documents and utilising the back button
+
     }
 }
